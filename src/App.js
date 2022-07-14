37266f1080843efe793c4669daaed279d80833c3
WiaanB <whoppingwaki@gmail.com>
2022-07-14 23:14:14 +0200

    added the display and logic for changing topics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import Banner from './Banner';
 import Menu from './Menu';
-import { useState } from 'react';
+import Display from './Display';
 
 function App() {
   const [showMenu, setshowMenu] = useState(false);
+  const [topic, setTopic] = useState({id:"bo8jQKTaE0Y"});
 
   function toggleMenu() {
     setshowMenu(!showMenu)
   }
 
+  function topicSelected(newTopic) {
+    setTopic(newTopic)
+    toggleMenu()
+  }
+
   return (
     <div className="App">
-      { showMenu && <Menu toggleMenu={toggleMenu}/> }
+      { showMenu && <Menu toggleMenu={toggleMenu} changeTopic={topicSelected}/> }
       <Banner toggleMenu={toggleMenu}/>
+      <Display topic={topic} showNav={showMenu}/>
     </div>
   );
 }
