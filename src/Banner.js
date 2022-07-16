4b5dc3e9064ee746d5dd4f91482a77f4834ab7b8
WiaanB <whoppingwaki@gmail.com>
2022-07-16 13:10:18 +0200

    stylized the logo

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,16 @@
+import { motion } from 'framer-motion';
+
 function Banner({ toggleMenu }) {
     return (
         <div className="top-banner">
             <i onClick={toggleMenu} className="fa fa-bars"></i>
-            <h1 className="title">Smart TV</h1>
+            <motion.h1
+                className="title"
+                initial={{ x: -100 }}
+                animate={{ x: 0 }}
+            >
+                Smart TV
+            </motion.h1>
         </div>
     );
 };
